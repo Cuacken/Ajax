0130f7ad821eba1e2acd5f10bfa12b375c0067a5
Cuacken <132932826+Cuacken@users.noreply.github.com>
2023-07-10 17:38:05 -0300

    repasando

--- js/JS.js
+++ js/JS.js
@@ -22,7 +22,31 @@ function buscar() {
   let tarjetas = document.querySelector("#ConteinerCard");
   var busqueda = document.querySelector("#nPokemon").value;
 
-  if (busqueda > 0) {
+
+  if(busqueda >= 906 || busqueda <=0){
+    
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * max);
+    }
+
+    let html2 =
+      `<div class="card" style="width: 18rem; background-color: rgba(255, 255, 255, 0); border: none;">
+    <img src="` +
+      "../img/MissingNO" + getRandomInt(14) + ".png" +
+      `" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">` +
+      "Missing Number" +
+      `</h5>
+      <p class="card-text"></p>
+      <img src="../img/unknown.png"
+      class="card-img-top" alt="...">
+<p class="card-text"></p>
+</div>
+</div>`;
+    tarjetas.innerHTML = html2;
+  }
+  else if (busqueda > 0 ) {
     var data = document.querySelector("#nPokemon").data;
     var url = data.results[busqueda - 1].url;
     var objXMLHttpRequest = new XMLHttpRequest();
@@ -31,10 +55,9 @@ function buscar() {
       if (objXMLHttpRequest.readyState === 4) {
         if (objXMLHttpRequest.status === 200) {
           let json = JSON.parse(objXMLHttpRequest.responseText);
-          let nombre = json.name;
+          let nombre = json.species.name;
           let uriImg = json.sprites.other.home.front_default;
           console.log(json);
-          
           let tipo1 = json.types[0].type.name;
           let tipo2 = undefined;
           if(json.types[1] != undefined){
@@ -49,7 +72,7 @@ function buscar() {
             `" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">` +
-            nombre +
+            nombre[0].toUpperCase() + nombre.substring(1) +
             `</h5>`
             +
             `<img src="../img/`+ tipo1 +`.png"
@@ -67,27 +90,7 @@ function buscar() {
     };
     objXMLHttpRequest.open("GET", url);
     objXMLHttpRequest.send();
-  }
-
-  if (busqueda <= 0) {
-
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    }
-
-    let html1 =
-      `<div class="card" style="width: 18rem; background-color: rgba(255, 255, 255, 0); border: none;">
-    <img src="` +
-      "../img/MissingNO" + getRandomInt(14) + ".png" +
-      `" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">` +
-      "Missing Number" +
-      `</h5>
-      <p class="card-text"></p>
-    </div>
-  </div>`;
-    tarjetas.innerHTML = html1;
+    
   }
 
 }
